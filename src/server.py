8c44180d970a644ba312937eefd390d58e19fa02
Mamuka Sakhelashvili <msakh12@freeuni.edu.ge>
2021-11-13 13:28:34 +0100

    Update server.py
    Updated route with home

--- src/server.py
+++ src/server.py
@@ -3,6 +3,6 @@ from flask import render_template
 
 app = Flask(__name__, template_folder='../templates')
 
-@app.route("/")
+@app.route("/home")
 def hello_world():
     return render_template('homePage.html')
